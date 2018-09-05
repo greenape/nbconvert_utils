ad3ab2bbd8c488dbe699672ac9d4e94045335dfc
Jono Gray <demonixz@gmail.com>
2018-09-05 21:44:45 +0100

    Slightly different approach

--- pandas_to_markdown/__init__.py
+++ pandas_to_markdown/__init__.py
@@ -11,5 +11,12 @@ def to_md(self):
 
 class PandasToMarkdown(nbconvert.preprocessors.Preprocessor):
     def preprocess(self, nb, resources):
+        print(resources)
         nb.display_formatter.formatters["text/html"].for_type(pd.DataFrame, to_md)
         return nb, resources
+
+from nbconvert.preprocessors.execute import ExecutePreprocessor
+from traitlets import List, Unicode
+
+class Preambler(ExecutePreprocessor):
+    extra_arguments = ["--profile-dir=/Users/jono/flowkit/docs/nbconvert_config/default"]
